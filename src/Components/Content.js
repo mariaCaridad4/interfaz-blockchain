ea022cb7c3eb7d1f7f062873db98df4e9390b6ec
mariaCaridad4 <mcaridad.caceres@gmail.com>
2021-06-07 17:36:54 -0500

    jwt-contenido paciente

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,10 +2,12 @@ import React, { useContex } from 'react';
 import { Redirect } from '@reach/router';
 import { UserContext } from '../App';
 
+import Paciente from '../NavPaciente';
+
 const Content = () => {
     const [user] = useContex(UserContext);
     if(!user.accestoken) return <Redirect from='' to='login' noThrow />
-    return <div>This is the content.</div>;
+    return <div><Paciente /></div>;
 }
 
 export default Content;
