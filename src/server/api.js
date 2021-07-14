da49cb0b5a915df1aaefdf51b6e5380b9620ded9
CookieDani <soyyo.peralta@gmail.com>
2021-07-13 20:06:20 -0500

    conectado a servidor de la u

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:3000/", //Ruta del backend
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://10.0.2.3:23000/",
   withCredentials: false, 
 });
 
