931c4cea543c37bdb282b6db63ea08bf018b09fc
CookieDani <soyyo.peralta@gmail.com>
2021-07-21 16:20:06 -0500

    cambio https server

--- src/server/api.js
+++ src/server/api.js
@@ -1,9 +1,16 @@
+import https from "https";
 import axios from "axios";
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// https.globalAgent.options.rejectUnauthorized = false;
 const api = axios.create({
   // baseURL: "http://localhost:3000/", //Ruta del backend
-  baseURL: "http://10.0.2.3:23000/",
-  withCredentials: false, 
+  baseURL: "https://localhost:23000/",
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  }),
 });
+
 
 
 
