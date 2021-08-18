c5ce9311813497bcd7546dfe0e71e7ed32df1d69
administrador <soyyo.peralta@gmail.com>
2021-08-18 04:28:41 +0000

    Cambio direccion de api del servidor

--- src/server/api.js
+++ src/server/api.js
@@ -5,7 +5,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 // https.globalAgent.options.rejectUnauthorized = false;
 const api = axios.create({
   // baseURL: "http://localhost:3000/", //Ruta del backend
-  baseURL: "https://localhost:23000/",
+  baseURL: "https://telemedicina.ucuenca.edu.ec/api/",
   httpsAgent: new https.Agent({  
     rejectUnauthorized: false
   }),
